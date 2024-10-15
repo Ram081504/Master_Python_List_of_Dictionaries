a58d8285d40e06df42e27551eeb828a3c7b1e731
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-10-15 23:37:10 +0800

    feat: Add Employee list of dictionary in python

---
+++ employee.py
@@ -0,0 +1,34 @@
+Employee_list = {
+ "Name": "Kisseraskley Endonela", 
+ "Job": "Web Designer",
+ "Number": "00015019",
+ "Age": 20,
+ "Email": "Endonela@gmail.com",
+}, {
+ "Name": "Mat Gabriel Sevella",
+ "Job": "SQL Developer",
+ "Number": "00015150",
+ "Age": 19,
+ "Email": "Sevella@gmail.com",
+}, {
+ "Name": "John Denson Inao",
+ "Job": "Web Developer",
+ "Number": "00015456",
+ "Age": 21,
+ "Email": "Inao@gmail.com",
+}, {
+ "Name": "Christian Toribio",
+ "Job": "Software Engineer",
+ "Number": "00015605",
+ "Age": 20,
+ "Email": "Toribio@gmail.com",
+}, {
+ "Name": "Ram Louise Capilitan", 
+ "Job": "IT Professional",
+ "Number": "00015607",
+ "Age": 20,
+ "Email": "Capilitan@gmail.com",
+}
+
+for Employee in Employee_list:
+   print(f"Name: {Employee['Name']}, Job: {Employee['Job']}, Number: {Employee['Number']}, Age: {Employee['Age']}, Email: {Employee['Email']}")
